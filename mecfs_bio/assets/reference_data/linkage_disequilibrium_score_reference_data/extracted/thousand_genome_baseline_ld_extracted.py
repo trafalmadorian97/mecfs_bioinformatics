e1ef932b71c0b22c33d907035d9634918a754d04
trafalmadorian97 <peterfor42@gmail.com>
2025-11-27 23:30:26 -0500

    wip

---
+++ mecfs_bio/assets/reference_data/linkage_disequilibrium_score_reference_data/extracted/thousand_genome_baseline_ld_extracted.py
@@ -0,0 +1,9 @@
+from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.raw.partitioned_model_baseline_ld_scores import \
+    THOUSAND_GENOME_BASE_MODEL_PARTITIONED_LD_SCORES
+from mecfs_bio.build_system.task.extract_tar_gzip_task import ExtractTarGzipTask
+
+BASE_MODEL_PARTITIONED_LD_SCORES_EXTRACTED = ExtractTarGzipTask.create_from_reference_file_task(
+    asset_id="base_model_partitioned_ld_scores_extracted",
+    source_task= THOUSAND_GENOME_BASE_MODEL_PARTITIONED_LD_SCORES ,
+    read_mode="r"
+)
