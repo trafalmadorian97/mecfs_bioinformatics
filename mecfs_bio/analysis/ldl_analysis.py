8e12bc96731c2b4817748e8740a01f261325f8ef
trafalmadorian97 <peterfor42@gmail.com>
2025-11-28 22:32:20 -0500

    uncomment

--- mecfs_bio/analysis/ldl_analysis.py
+++ mecfs_bio/analysis/ldl_analysis.py
@@ -2,6 +2,9 @@ from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
 from mecfs_bio.assets.gwas.ldl.million_veterans.analysis.ldl_base_cell_analysis_by_ldsc import (
     MILLION_VETERAN_LDL_BASE_CELL_ANALYSIS_BY_LDSC,
 )
+from mecfs_bio.assets.gwas.ldl.million_veterans.processed_gwas_data.million_veterans_ldl_eur_magma_task_generator import (
+    MILLION_VETERANS_EUR_LDL_MAGMA_TASKS,
+)
 
 
 def run_ldl_analysis():
@@ -14,8 +17,8 @@ def run_ldl_analysis():
     """
     DEFAULT_RUNNER.run(
         [
-            # MILLION_VETERANS_EUR_LDL_MAGMA_TASKS.inner.bar_plot_task,
-            MILLION_VETERAN_LDL_BASE_CELL_ANALYSIS_BY_LDSC
+            MILLION_VETERANS_EUR_LDL_MAGMA_TASKS.inner.bar_plot_task,
+            MILLION_VETERAN_LDL_BASE_CELL_ANALYSIS_BY_LDSC,
         ],
         incremental_save=True,
         # must_rebuild_transitive=[
