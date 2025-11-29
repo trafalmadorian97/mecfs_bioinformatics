4232e828faf59eabf8fc5d8b6941ac87d88b9910
trafalmadorian97 <peterfor42@gmail.com>
2025-11-28 22:25:55 -0500

    format

--- mecfs_bio/analysis/partitioned_ld_ref_data.py
+++ mecfs_bio/analysis/partitioned_ld_ref_data.py
@@ -1,25 +1,24 @@
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
-from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.extracted.partitioned_model_multi_tissue_gene_expr_ld_score_extracted import \
-    PARTITIONED_MODEL_MULTI_TISSUE_GENE_EXPR_LD_SCORES_EXTRACTED
-from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.extracted.partitioned_model_regression_weights_extracted import \
-    PARTITIONED_MODEL_REGRESSION_WEIGHTS_EXTRACTED
-from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.extracted.thousand_genome_baseline_ld_extracted import \
-    BASE_MODEL_PARTITIONED_LD_SCORES_EXTRACTED
-from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.raw.partitioned_model_baseline_ld_scores import (
-    THOUSAND_GENOME_BASE_MODEL_PARTITIONED_LD_SCORES,
+from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.extracted.partitioned_model_multi_tissue_gene_expr_ld_score_extracted import (
+    PARTITIONED_MODEL_MULTI_TISSUE_GENE_EXPR_LD_SCORES_EXTRACTED,
+)
+from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.extracted.partitioned_model_regression_weights_extracted import (
+    PARTITIONED_MODEL_REGRESSION_WEIGHTS_EXTRACTED,
+)
+from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.extracted.thousand_genome_baseline_ld_extracted import (
+    BASE_MODEL_PARTITIONED_LD_SCORES_EXTRACTED,
 )
 
 
 def run_initial_analysis():
-    DEFAULT_RUNNER.run([
-
-        #THOUSAND_GENOME_BASE_MODEL_PARTITIONED_LD_SCORES,
-
-        PARTITIONED_MODEL_MULTI_TISSUE_GENE_EXPR_LD_SCORES_EXTRACTED,
-        BASE_MODEL_PARTITIONED_LD_SCORES_EXTRACTED,
-        PARTITIONED_MODEL_REGRESSION_WEIGHTS_EXTRACTED
-
-                        ])
+    DEFAULT_RUNNER.run(
+        [
+            # THOUSAND_GENOME_BASE_MODEL_PARTITIONED_LD_SCORES,
+            PARTITIONED_MODEL_MULTI_TISSUE_GENE_EXPR_LD_SCORES_EXTRACTED,
+            BASE_MODEL_PARTITIONED_LD_SCORES_EXTRACTED,
+            PARTITIONED_MODEL_REGRESSION_WEIGHTS_EXTRACTED,
+        ]
+    )
 
 
 if __name__ == "__main__":
--- test_mecfs_bio/unit/build_system/task/gwaslab/test_gwaslab_cell_analysis_by_ldsc.py
+++ test_mecfs_bio/unit/build_system/task/gwaslab/test_gwaslab_cell_analysis_by_ldsc.py
@@ -1,12 +1,13 @@
-from pathlib import Path
-
-from mecfs_bio.build_system.task.gwaslab.gwaslab_cell_analysis_by_ldsc import ldcts_raw_to_processed, \
-    ldcts_processed_to_raw
+from mecfs_bio.build_system.task.gwaslab.gwaslab_cell_analysis_by_ldsc import (
+    ldcts_processed_to_raw,
+    ldcts_raw_to_processed,
+)
 
 _sample_ldcts_file_contents = """Adipose_Subcutaneous\tMulti_tissue_gene_expr_1000Gv3_ldscores/GTEx.1.,Multi_tissue_gene_expr_1000Gv3_ldscores/GTEx.control.
 Adipose_Visceral_(Omentum)\tMulti_tissue_gene_expr_1000Gv3_ldscores/GTEx.2.,Multi_tissue_gene_expr_1000Gv3_ldscores/GTEx.control.
 Adrenal_Gland\tMulti_tissue_gene_expr_1000Gv3_ldscores/GTEx.3.,Multi_tissue_gene_expr_1000Gv3_ldscores/GTEx.control.
 Artery_Aorta\tMulti_tissue_gene_expr_1000Gv3_ldscores/GTEx.4.,Multi_tissue_gene_expr_1000Gv3_ldscores/GTEx.control."""
+
 
 def test_round_trip_ldcts_file_contents():
     processed = ldcts_raw_to_processed(_sample_ldcts_file_contents)
