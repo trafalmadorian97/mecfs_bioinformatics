12e1f4d1d600244eb84a35d5e1d58642831bcf71
trafalmadorian97 <peterfor42@gmail.com>
2025-11-28 20:33:28 -0500

    clean

--- test/unit/data_processing/path_constants.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-DUMMY_DATA_PATH = Path("test/unit/data_processing/dummy_data.regenie")
-DUMMY_DATA_PATH_WITH_INDEL = Path(
-    "test/unit/data_processing/dummy_data_with_indel.regenie"
-)
