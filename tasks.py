e297eb166677cf253c5304110b601f0a3ab74803
Tralfamadoran97 <peterfor42@gmail.com>
2025-11-28 20:43:41 -0500

    Delete old `src` and `test` folders (#184)
    - The old version of the framework lived in the `src` folder, and was
    tested by the `test` folder
    - I have since replicated most of the functionality of the old frame
    with my new build-system-based framework that lives in `mecfs_bio` and
    is tested by `test_mecf_bio`. Thus the old code is mostly redundant.
    - This PR cleans the repo up by deleting this old cose.
    - Also, previously `tasks.py` contained code for downloading `DecodeME`
    data files. This is no longer needed, as I have a `task` in `mecfs_bio`
    to download this data. So I cleaned up `tasks.py` by deleteing this.

--- tasks.py
+++ tasks.py
@@ -2,14 +2,6 @@ from pathlib import Path
 
 from invoke import task
 
-from src.data_processing_util.gzip_util import copy_extract
-
-DECODE_ME_PROJECT_ID = "rgqs3"
-DECODE_ME_RAW_DATA_PATH = Path("data/DecodeME/raw")
-DECODE_ME_EXTRACTED_DATA_PATH = Path("data/DecodeME/extracted")
-OSF_FILE_NAME = "osfstorage"
-DECODE_ME_RAW_OSF_DATA_PATH = DECODE_ME_RAW_DATA_PATH / OSF_FILE_NAME
-UNIT_TEST_PATH = Path("test/unit")
 NEW_UNIT_TEST_PATH = Path("test_mecfs_bio/unit")
 
 
@@ -17,7 +9,7 @@ NEW_UNIT_TEST_PATH = Path("test_mecfs_bio/unit")
 @task
 def test(c):
     print("Running unit and integration tests with pytest")
-    c.run(f"pixi r python  -m pytest {UNIT_TEST_PATH} {NEW_UNIT_TEST_PATH}", pty=True)
+    c.run(f"pixi r python  -m pytest  {NEW_UNIT_TEST_PATH}", pty=True)
 
 
 @task
@@ -68,31 +60,5 @@ def green(c):
 
 
 @task
-def get_eur_1k_genomes_gwaslab(c):
-    import gwaslab as gl
-
-    from src.data_processing.using_gwaslab.gwaslab_constants import (
-        GWASLAB_EUR_1K_GENOMES_NAME_38,
-    )
-
-    gl.download_ref(GWASLAB_EUR_1K_GENOMES_NAME_38)
-
-
-@task
 def install_tabix_ubuntu(c):
     c.run("sudo apt-get install tabix")
-
-
-## Data tasks
-
-
-@task
-def pull_Decode_ME(c):
-    print("Pulling DecodeME data...")
-    c.run(f"pixi r python -p {DECODE_ME_PROJECT_ID} clone -U {DECODE_ME_RAW_DATA_PATH}")
-
-
-@task
-def extract_Decode_ME(c):
-    print("Extracting DecodeME data...")
-    copy_extract(DECODE_ME_RAW_DATA_PATH, DECODE_ME_EXTRACTED_DATA_PATH)
