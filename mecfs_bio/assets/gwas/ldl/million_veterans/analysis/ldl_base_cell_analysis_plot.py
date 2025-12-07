0d070f4fea1b5d8a774d863764d018e8336d1989
Tralfamadoran97 <peterfor42@gmail.com>
2025-12-07 18:27:01 -0500

    Refactoring of ldl tasks (#210)
    - Use task generator to generate S-LDSC related tasks for the LDL GWAS.
    - Delete old individual S-LSDC LDL task objects, since they are replaced
    by the task generator

--- mecfs_bio/assets/gwas/ldl/million_veterans/analysis/ldl_base_cell_analysis_plot.py
+++
@@ -1,9 +0,0 @@
-from mecfs_bio.assets.gwas.ldl.million_veterans.analysis.ldl_base_cell_analysis_add_categories import (
-    MILLION_VETERAN_LDL_BASE_CELL_ANALYSIS_BY_LDSC_LABELED,
-)
-from mecfs_bio.build_system.task.sldsc_scatter_plot_task import SLDSCScatterPlotTask
-
-LDL_BASE_CELL_PLOT = SLDSCScatterPlotTask.create(
-    asset_id="ldl_base_cell_analysis_plot",
-    source_task=MILLION_VETERAN_LDL_BASE_CELL_ANALYSIS_BY_LDSC_LABELED,
-)
