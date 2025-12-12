fbc97bd220ea2f01144010feddf83437698f347b
Tralfamadoran97 <peterfor42@gmail.com>
2025-12-12 15:20:19 -0500

    move_ibd_files (#228)
    - Refactor the directory containing tasks related to the IBD GWAS

--- mecfs_bio/assets/gwas/inflammatory_bowel_disease/liu_et_al_2023/analysis/magma/liu_et_al_2023_eur_37_specific_tissue_bar_plot.py
+++ mecfs_bio/assets/gwas/inflammatory_bowel_disease/liu_et_al_2023/analysis/magma/liu_et_al_2023_eur_37_specific_tissue_bar_plot.py
@@ -1,4 +1,4 @@
-from mecfs_bio.assets.gwas.inflammatory_bowel_disease.liu_et_al_2023.processed_gwas_data.analysis.magma.liu_et_al_2023_eur_37_magma_ensembl_specific_tisssue_gene_sets import (
+from mecfs_bio.assets.gwas.inflammatory_bowel_disease.liu_et_al_2023.analysis.magma.liu_et_al_2023_eur_37_magma_ensembl_specific_tisssue_gene_sets import (
     LIU_ET_AL_IBD_EUR_37_SPECIFIC_TISSUE_GENE_SET_ANALYSIS,
 )
 from mecfs_bio.build_system.task.magma.magma_plot_gene_set_result import (
