c19446469f86b4cffab36e47d410b74eee6ac986
trafalmadorian97 <peterfor42@gmail.com>
2025-12-13 17:25:54 -0500

    wip

---
+++ mecfs_bio/assets/reference_data/db_snp/dbsnp_151_build_37.py
@@ -0,0 +1,17 @@
+from pathlib import PurePath
+
+from mecfs_bio.build_system.meta.reference_meta.reference_file_meta import ReferenceFileMeta
+from mecfs_bio.build_system.task.download_file_task import DownloadFileTask
+
+DBSNP_BUILD_37_VCF_FILE=DownloadFileTask(
+    meta=ReferenceFileMeta(
+        group="db_snp_reference_data",
+        sub_group="build_37",
+        sub_folder=PurePath("vcf"),
+        asset_id="db_snp_vcf_reference_file_build_37",
+        extension=".gz",
+        filename="GCF_000001405.25",
+    ),
+    url="https://ftp.ncbi.nih.gov/snp/latest_release/VCF/GCF_000001405.25.gz",
+    md5_hash="35db22bcd166f904e4775dbbc29f5965"
+)
