462956e2e15462ec89048e19a5ac1b733ae967e2
trafalmadorian97 <peterfor42@gmail.com>
2025-12-13 21:58:17 -0500

    wip

---
+++ mecfs_bio/assets/reference_data/db_snp/db_snp_vcf_file_for_rsid_assignment_build_37.py
@@ -0,0 +1,22 @@
+from pathlib import PurePath
+
+from src_new.build_system.meta.reference_meta.reference_file_meta import (
+    ReferenceFileMeta,
+)
+from src_new.build_system.task.reference.download_file_task import DownloadFileTask
+
+"""
+# see: https://cloufield.github.io/gwaslab/AssignrsID/
+"""
+DB_SNP_VCF_FILE_BUILD_37 = DownloadFileTask(
+    meta=ReferenceFileMeta(
+        group="db_snp_reference_data",
+        sub_group="build_37",
+        sub_folder=PurePath("vcf"),
+        asset_id="db_snp_vcf_reference_file_build_37",
+        filename="GCF_000001405.25",
+        extension=".gz",
+    ),
+    url="https://ftp.ncbi.nih.gov/snp/latest_release/VCF/GCF_000001405.25.gz",
+    md5_hash="35db22bcd166f904e4775dbbc29f5965",
+)
