51794ead18920c7082c841f70065101d14a7fbcd
trafalmadorian97 <peterfor42@gmail.com>
2026-01-03 22:55:40 -0500

    brain_atlas

--- mecfs_bio/analysis/ref_data.py
+++ mecfs_bio/analysis/ref_data.py
@@ -3,9 +3,8 @@ Script to download some reference data.  Mainly for testing.
 """
 
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
-from mecfs_bio.assets.reference_data.pqtls.processed.sun_et_al_2023_pqtls_discovery_extracted import (
-    SUN_ET_AL_2023_DICOVERY_PQTLS_EXTRACTED,
-)
+from mecfs_bio.assets.reference_data.magma_specificity_matricies.raw.magma_specificity_matrix_from_hbca_rna_duncan import \
+    MAGMA_ENTREZ_SPECIFICITY_MATRIX_HBCA_RNA_DUNCAN
 
 
 def run_initial_analysis():
@@ -25,7 +24,8 @@ def run_initial_analysis():
             # PARQUET_DBSNP150_37_ANNOVAR_PROC_RENAME_UNIQUE
             # GENE_THESAURUS,
             # SUN_ET_AL_2023_PQTL,
-            SUN_ET_AL_2023_DICOVERY_PQTLS_EXTRACTED
+            # SUN_ET_AL_2023_DICOVERY_PQTLS_EXTRACTED
+            MAGMA_ENTREZ_SPECIFICITY_MATRIX_HBCA_RNA_DUNCAN
         ],
         incremental_save=True,
         # must_rebuild_transitive=[PARQUET_DBSNP_37_UNNESTED],
