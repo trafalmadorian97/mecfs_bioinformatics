54f95212c0d920d89ec1ac8a04d605f44b1d935b
trafalmadorian97 <peterfor42@gmail.com>
2026-01-11 18:52:54 -0500

    revert dbp analysis

--- mecfs_bio/analysis/dbp_analysis.py
+++ mecfs_bio/analysis/dbp_analysis.py
@@ -6,6 +6,9 @@ from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
 from mecfs_bio.assets.gwas.blood_pressure.keaton_et_al_diastolic.analysis.keaton_dbp_standard_analysis import (
     KEATON_DBP_STANDARD_ANALYSIS,
 )
+from mecfs_bio.assets.gwas.blood_pressure.keaton_et_al_diastolic.raw.raw_dbp_data import (
+    KEATON_ET_AL_DBP_RAW,
+)
 
 
 def run_dbp_analysis():
@@ -14,12 +17,8 @@ def run_dbp_analysis():
 
     """
     DEFAULT_RUNNER.run(
-        # [KEATON_ET_AL_DBP_RAW] + KEATON_DBP_STANDARD_ANALYSIS.get_terminal_tasks(),
-        [KEATON_DBP_STANDARD_ANALYSIS.hba_magma_tasks.magma_independent_cluster_plot],
+        [KEATON_ET_AL_DBP_RAW] + KEATON_DBP_STANDARD_ANALYSIS.get_terminal_tasks(),
         incremental_save=True,
-        must_rebuild_transitive=[
-            KEATON_DBP_STANDARD_ANALYSIS.hba_magma_tasks.magma_independent_cluster_plot
-        ],
         # must_rebuild_transitive=[KEATON_DBP_STANDARD_ANALYSIS.magma_tasks.inner.bar_plot_task]
     )
 
