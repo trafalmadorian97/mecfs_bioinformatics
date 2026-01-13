78678071ccda33cd7750aeb3305ce599cfc5327a
trafalmadorian97 <peterfor42@gmail.com>
2026-01-13 18:30:23 -0500

    ibd_hba

--- mecfs_bio/assets/gwas/inflammatory_bowel_disease/liu_et_al_2023/analysis/magma/liu_et_al_2023_eur_37_hba_magma_analysis.py
+++ mecfs_bio/assets/gwas/inflammatory_bowel_disease/liu_et_al_2023/analysis/magma/liu_et_al_2023_eur_37_hba_magma_analysis.py
@@ -5,6 +5,9 @@ from mecfs_bio.assets.gwas.inflammatory_bowel_disease.liu_et_al_2023.processed_g
     LIU_ET_AL_2023_ASSIGN_RSID_VIA_SNP150_ANNOVAR,
 )
 from mecfs_bio.build_system.task.gwaslab.gwaslab_constants import GWASLAB_RSID_COL
+from mecfs_bio.build_system.task.magma.magma_plot_brain_atlas_result_with_stepwise_labels import (
+    HBAIndepPlotOptions,
+)
 from mecfs_bio.build_system.task.magma.plot_magma_brain_atlas_result import PlotSettings
 from mecfs_bio.build_system.task.pipes.rename_col_pipe import RenameColPipe
 
@@ -16,4 +19,6 @@ IBD_HBA_MAGMA_TASKS = generate_human_brain_atlas_magma_tasks(
     pipes=[
         RenameColPipe(old_name="rsid", new_name=GWASLAB_RSID_COL),
     ],
+    include_independent_cluster_plot=True,
+    hba_indep_plot_options=HBAIndepPlotOptions(annotation_text_size=16),
 )
