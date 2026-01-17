2426f06da16c486151a74527863cc59c7c9f4591
Tralfamadoran97 <peterfor42@gmail.com>
2026-01-17 12:36:27 -0500

    Uniprot Reference Data (#346)
    - Add a task class for downloading reference data about proteins from
    uniprot.
    -Add a concrete instantiation of this class

--- mecfs_bio/analysis/asthma.py
+++ mecfs_bio/analysis/asthma.py
@@ -3,9 +3,6 @@ Script to analyze asthma phenotype.
 """
 
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
-from mecfs_bio.assets.gwas.asthma.han_et_al_2022.analysis.asthma_ppp_two_sample_mr import (
-    HAN_2022_ASTHMA_TSMR,
-)
 
 
 def run_initial_asthma_analysis():
@@ -15,12 +12,13 @@ def run_initial_asthma_analysis():
     - MAGMA and S-LDSC analysis
     """
     DEFAULT_RUNNER.run(
-        [HAN_2022_ASTHMA_TSMR],
+        [],
+        # [HAN_2022_ASTHMA_TSMR],
         # [HAN_ET_AL_ASTHMA_RAW] + HAN_ASTHMA_STANDARD_ANALYSIS.get_terminal_tasks(),
         incremental_save=True,
         must_rebuild_transitive=[
             # HAN_ASTHMA_STANDARD_ANALYSIS.labeled_lead_variant_tasks.raw_sumstats_task
-            HAN_2022_ASTHMA_TSMR
+            # HAN_2022_ASTHMA_TSMR
         ],
     )
 
