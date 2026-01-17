5fd58acc395d03c247dcbeb5e885409128da0a21
trafalmadorian97 <peterfor42@gmail.com>
2026-01-17 16:19:52 -0500

    lookup

--- mecfs_bio/assets/reference_data/uniprot/uniprot_lookup_table.py
+++ mecfs_bio/assets/reference_data/uniprot/uniprot_lookup_table.py
@@ -1,3 +1,6 @@
+"""
+Task to download reference information about proteins from UniProt
+"""
 from mecfs_bio.build_system.task.get_uniprot_reference_data_task import (
     GetUniProtReferenceDataTask,
 )
