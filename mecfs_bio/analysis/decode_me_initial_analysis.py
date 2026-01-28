a3f5a4b2b47872e2df1d1934811a96a224ced0b4
trafalmadorian97 <peterfor42@gmail.com>
2026-01-27 21:34:19 -0500

    fix

--- mecfs_bio/analysis/decode_me_initial_analysis.py
+++ mecfs_bio/analysis/decode_me_initial_analysis.py
@@ -3,6 +3,9 @@ Script to run initial analysis on DecodeME data.
 """
 
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
+from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_gwas_1_cis_pqtl_mr import (
+    DECODE_ME_BASIC_CIS_PQTL_MR,
+)
 from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_gwas_1_combined_gene_list_markdown import (
     DECODE_ME_MASTER_GENE_LIST_AS_MARKDOWN,
 )
@@ -23,9 +26,6 @@ from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.magma.decode_me_hba_magma_a
 )
 from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.magma.magma_specific_tissue_bar_plot import (
     MAGMA_DECODE_ME_SPECIFIC_TISSUE_ANALYSIS_BAR_PLOT,
-)
-from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_gwas_1_cis_pqtl_mr import (
-    DECODE_ME_BASIC_CIS_PQTL_MR,
 )
 
 
