d29ebe63e8e2788c2319f047c853ff94938858a8
trafalmadorian97 <peterfor42@gmail.com>
2026-01-28 17:20:23 -0500

    wip

--- mecfs_bio/assets/reference_data/ukbb_ppp_sumstats/rabgap1l/processed/ukbb_rabgap1l_sumstats_37.py
+++ mecfs_bio/assets/reference_data/ukbb_ppp_sumstats/rabgap1l/processed/ukbb_rabgap1l_sumstats_37.py
@@ -8,18 +8,12 @@ from mecfs_bio.build_system.task.gwaslab.gwaslab_create_sumstats_task import (
     HarmonizationOptions,
 )
 
-UKBBPPP_RABGAP1l_SUMSTATS_37_HARMONIZED = GWASLabCreateSumstatsTask(
+UKBBPPP_RABGAP1l_SUMSTATS_37 = GWASLabCreateSumstatsTask(
     df_source_task=STACK_UKBBPPP_RABGAP1L,
     asset_id=AssetId("sumstats_37_ukbb_rabgap1l_gwaslab"),
     basic_check=True,
     genome_build="infer",
     liftover_to="19",
     fmt="regenie",
-    harmonize_options=HarmonizationOptions(
-        ref_infer=GWASLabVCFRef(name="1kg_eur_hg19", ref_alt_freq="AF"),
-        ref_seq="ucsc_genome_hg19",
-        check_ref_files=True,
-        drop_missing_from_ref=True,
-        cores=4,
-    ),
+    harmonize_options=None,
 )
