6d52884ca44784121779b2763e010e99f0fbe51f
Tralfamadoran97 <peterfor42@gmail.com>
2026-01-31 17:40:55 -0500

    Update __init__.py

--- mecfs_bio/assets/reference_data/ukbb_ld_matrices/__init__.py
+++ mecfs_bio/assets/reference_data/ukbb_ld_matrices/__init__.py
@@ -1,7 +1,8 @@
 """
 Tasks for downloading LD matrices generated from UK Biobank data.
 
-Having a large, population-matched LD matrix is important for multiple-causal variant fine-mapping
+Having a large, population-matched LD matrix is important for multiple-causal variant fine-mapping.
 
-Hilary Finucane briefly discusses the importance of having an appropriate LD matrix in this broad institute talk: https://www.youtube.com/watch?v=S6vfOr336b0
+Hilary Finucane briefly discusses the importance of having an appropriate LD matrix in this Broad institute talk: https://www.youtube.com/watch?v=S6vfOr336b0
 """
+
