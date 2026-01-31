7b28f0605ad44abc1199a9cce6795b2a8416ce2b
ruvilonix <114708614+ruvilonix@users.noreply.github.com>
2026-01-30 22:49:01 -0500

    Add tasks to do MAGMA while controlling for average expression of only brain tissues. (#402)
    This PR adds tasks which are almost identical to the existing MAGMA
    tissue gene set analysis and tissue bar plot tasks. The only change is
    that instead of creating a covariate of each gene's average expression
    across all 68 GTEx body tissues, it creates an average using only brain
    tissues.
    
    The idea is that gene expression is very correlated among brain tissues,
    in comparison to correlation of a given brain tissue with a non-brain
    tissue (see for example, [Fig 3A from Urbut
    2019](https://pmc.ncbi.nlm.nih.gov/articles/PMC6309609/figure/F3/)).
    Thus, even when controlling for average expression across all body
    tissues, multiple non-causal brain tissues may still be significant
    simply due to their high correlation to a different causal brain tissue.
    
    By controlling for average expression across brain tissues, we can see
    if expression in any brain tissues is associated with a phenotype, and
    which is not simply due to general brain-wide expression. This may
    potentially narrow down which specific tissue may be causal.
    
    ---------
    
    Co-authored-by: trafalmadorian97 <peterfor42@gmail.com>

---
+++ mecfs_bio/assets/reference_data/rna_seq_data/gtex_v10_median_tissue_expression_rna_seq_prep_for_magma_brain_average.py
@@ -0,0 +1,97 @@
+"""
+Analogous to GTEx_V10_MEDIAN_TISSUE_EXPRESSION_RNA_SEQ_PREP_FOR_MAGMA, but average is across brain tissue.
+By controlling for average expression across brain tissues, we can see if expression in any brain tissues is associated with a phenotype, and which is not simply due to general brain-wide expression. This may potentially narrow down which specific tissue may be causal.
+"""
+
+from mecfs_bio.assets.reference_data.rna_seq_data.gtex_v10_median_tissue_expression_rna_seq_raw import (
+    GTEx_V10_MEDIAN_TISSUE_EXPRESSION_RNA_SEQ,
+)
+from mecfs_bio.build_system.task.pipe_dataframe_task import (
+    CSVOutFormat,
+    PipeDataFrameTask,
+)
+from mecfs_bio.build_system.task.pipes.add_average_pipe import AddAveragePipe
+from mecfs_bio.build_system.task.pipes.drop_col_pipe import DropColPipe
+from mecfs_bio.build_system.task.pipes.filter_rows_by_min import FilterRowsByMin
+from mecfs_bio.build_system.task.pipes.move_col_to_front_pipe import MoveColToFrontPipe
+from mecfs_bio.build_system.task.pipes.shifted_log_pipe import ShiftedLogPipe
+from mecfs_bio.build_system.task.pipes.str_split_exact_col import SplitExactColPipe
+from mecfs_bio.build_system.task.pipes.winsorize_all import WinsorizeAllPipe
+
+GTEx_V10_MEDIAN_TISSUE_EXPRESSION_RNA_SEQ_PREP_FOR_MAGMA_BRAIN_AVERAGE = PipeDataFrameTask.create(
+    source_task=GTEx_V10_MEDIAN_TISSUE_EXPRESSION_RNA_SEQ,
+    asset_id="gtex_v10_rna_seq_median_tissue_expression_prep_for_magma_brain_average",
+    out_format=CSVOutFormat(sep="\t"),
+    pipes=[
+        DropColPipe(cols_to_drop=["Description"]),
+        FilterRowsByMin(min_value=1, exclude_columns=["Name"]),
+        WinsorizeAllPipe(max_value=50, cols_to_exclude=["Name"]),
+        SplitExactColPipe(
+            col_to_split="Name", split_by=".", new_col_names=("Gene", "Version")
+        ),
+        DropColPipe(cols_to_drop=["Version", "Name"]),
+        ShiftedLogPipe(cols_to_exclude=["Gene"], base=2, pseudocount=1),
+        AddAveragePipe(
+            cols_to_exclude=[
+                "Gene",
+                "Adipose_Subcutaneous",
+                "Adipose_Visceral_Omentum",
+                "Adrenal_Gland",
+                "Artery_Aorta",
+                "Artery_Coronary",
+                "Artery_Tibial",
+                "Bladder",
+                "Breast_Mammary_Tissue",
+                "Cells_Cultured_fibroblasts",
+                "Cells_EBV-transformed_lymphocytes",
+                "Cervix_Ectocervix",
+                "Cervix_Endocervix",
+                "Colon_Sigmoid",
+                "Colon_Transverse",
+                "Colon_Transverse_Mixed_Cell",
+                "Colon_Transverse_Mucosa",
+                "Colon_Transverse_Muscularis",
+                "Esophagus_Gastroesophageal_Junction",
+                "Esophagus_Mucosa",
+                "Esophagus_Muscularis",
+                "Fallopian_Tube",
+                "Heart_Atrial_Appendage",
+                "Heart_Left_Ventricle",
+                "Kidney_Cortex",
+                "Kidney_Medulla",
+                "Liver",
+                "Liver_Hepatocyte",
+                "Liver_Mixed_Cell",
+                "Liver_Portal_Tract",
+                "Lung",
+                "Minor_Salivary_Gland",
+                "Muscle_Skeletal",
+                "Nerve_Tibial",
+                "Ovary",
+                "Pancreas",
+                "Pancreas_Acini",
+                "Pancreas_Islets",
+                "Pancreas_Mixed_Cell",
+                "Pituitary",
+                "Prostate",
+                "Skin_Not_Sun_Exposed_Suprapubic",
+                "Skin_Sun_Exposed_Lower_leg",
+                "Small_Intestine_Terminal_Ileum",
+                "Small_Intestine_Terminal_Ileum_Lymphode_Aggregate",
+                "Small_Intestine_Terminal_Ileum_Mixed_Cell",
+                "Spleen",
+                "Stomach",
+                "Stomach_Mixed_Cell",
+                "Stomach_Mucosa",
+                "Stomach_Muscularis",
+                "Testis",
+                "Thyroid",
+                "Uterus",
+                "Vagina",
+                "Whole_Blood",
+            ],
+        ),
+        MoveColToFrontPipe(target_col="Gene"),
+    ],
+    backend="polars",
+)
