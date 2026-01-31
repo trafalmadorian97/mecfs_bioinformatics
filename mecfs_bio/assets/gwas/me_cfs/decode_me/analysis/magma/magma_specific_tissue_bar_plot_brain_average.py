1b010aac388d03fa19894be5a8d06970b6bcbcd5
Tralfamadoran97 <peterfor42@gmail.com>
2026-01-30 22:25:44 -0500

    Update magma_specific_tissue_bar_plot_brain_average.py

--- mecfs_bio/assets/gwas/me_cfs/decode_me/analysis/magma/magma_specific_tissue_bar_plot_brain_average.py
+++ mecfs_bio/assets/gwas/me_cfs/decode_me/analysis/magma/magma_specific_tissue_bar_plot_brain_average.py
@@ -1,5 +1,5 @@
 """
-Analogous to MAGMA_DECODE_ME_SPECIFIC_TISSUE_GENE_SET_ANALYSIS, but controls for average across brain tissue, not all tissues.
+Analogous to MAGMA_DECODE_ME_SPECIFIC_TISSUE_ANALYSIS_BAR_PLOT_BRAIN_AVERAGE, but controls for average across brain tissue, not all tissues.
 """
 
 from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.magma.decode_me_gwas_1_build_37_magma_ensembl_specific_tissue_gene_sets_brain_average import (
@@ -13,3 +13,4 @@ MAGMA_DECODE_ME_SPECIFIC_TISSUE_ANALYSIS_BAR_PLOT_BRAIN_AVERAGE = MAGMAPlotGeneS
     gene_set_analysis_task=MAGMA_DECODE_ME_SPECIFIC_TISSUE_GENE_SET_ANALYSIS_BRAIN_AVERAGE,
     asset_id="decode_me_gwas_1_build_37_magma_ensemble_specific_tissue_gene_covar_analysis_bar_plot_brain_average",
 )
+
