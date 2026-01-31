4c5c486b093e0d86b1960c4bb39744cca71913e3
Tralfamadoran97 <peterfor42@gmail.com>
2026-01-31 18:20:57 -0500

    Add a task to download an LD matrix constructed from UK Biobank. (#408)
    - Add task to download an LD matrix from UK biobank, as supplied by
    polyfun, See:
    https://github.com/omerwe/polyfun/wiki/3.-Functionally-informed-fine-mapping-with-finemapper.
    - Next step: I plan to use this LD matrix to test out fine-mapping.
    - Possible future steps: need to figure out how reformat these matrices
    to work with MAGMA>

---
+++ mecfs_bio/assets/reference_data/ukbb_ld_matrices/__init__.py
@@ -0,0 +1,7 @@
+"""
+Tasks for downloading LD matrices generated from UK Biobank data.
+
+Having a large, population-matched LD matrix is important for multiple-causal variant fine-mapping.
+
+Hilary Finucane briefly discusses the importance of having an appropriate LD matrix in this Broad Institute talk: https://www.youtube.com/watch?v=S6vfOr336b0
+"""
---
+++ mecfs_bio/assets/reference_data/ukbb_ld_matrices/from_polyfun/chr1_174000001_177000001_labels.py
@@ -0,0 +1,20 @@
+from pathlib import PurePath
+
+from mecfs_bio.build_system.meta.asset_id import AssetId
+from mecfs_bio.build_system.meta.reference_meta.reference_file_meta import (
+    ReferenceFileMeta,
+)
+from mecfs_bio.build_system.task.download_file_task import DownloadFileTask
+
+CHR1_174000001_17700000_UKBB_LD_LABELS_DOWNLOAD = DownloadFileTask(
+    meta=ReferenceFileMeta(
+        group="ukbb_reference_ld",
+        sub_group="chr1_174000001_17700000",
+        sub_folder=PurePath("raw"),
+        asset_id=AssetId("ukbb_chr1_174000001_17700000_ld_labels"),
+        filename="chr1_174000001_177000001",
+        extension=".gz",
+    ),
+    url="https://broad-alkesgroup-ukbb-ld.s3.amazonaws.com/UKBB_LD/chr1_174000001_177000001.gz",
+    md5_hash=None,
+)
