c8422d94be61aacd780813b7fdc980938fdc27d6
trafalmadorian97 <peterfor42@gmail.com>
2026-02-07 14:52:36 -0500

    wip

--- mecfs_bio/assets/reference_data/linkage_disequilibrium_score_reference_data/extracted/partitioned_model_multi_tissue_chromatin_ld_scores_extracted.py
+++ mecfs_bio/assets/reference_data/linkage_disequilibrium_score_reference_data/extracted/partitioned_model_multi_tissue_chromatin_ld_scores_extracted.py
@@ -1,6 +1,7 @@
 from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.raw.partitioned_model_multi_tissue_chromatin_ld_scores import (
     PARTITIONED_MODEL_MULTI_TISSUE_CHROMATIN_LD_SCORES_RAW,
 )
+from mecfs_bio.build_system.task.discard_deps_task_wrapper import DiscardDepsWrapper
 from mecfs_bio.build_system.task.extract_tar_gzip_task import ExtractTarGzipTask
 
 PARTITIONED_MODEL_MULTI_TISSUE_CHROMATIN_LD_SCORES_EXTRACTED = (
@@ -9,4 +10,7 @@ PARTITIONED_MODEL_MULTI_TISSUE_CHROMATIN_LD_SCORES_EXTRACTED = (
         source_task=PARTITIONED_MODEL_MULTI_TISSUE_CHROMATIN_LD_SCORES_RAW,
         read_mode="r",
     )
+)
+PARTITIONED_MODEL_MULTI_TISSUE_CHROMATIN_LD_SCORES_EXTRACTED_RD = DiscardDepsWrapper(
+    PARTITIONED_MODEL_MULTI_TISSUE_CHROMATIN_LD_SCORES_EXTRACTED
 )
