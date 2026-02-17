1933767f38fca29ce40fa6519e75220e65fecb1f
trafalmadorian97 <peterfor42@gmail.com>
2026-02-17 13:27:54 -0500

    wip

--- experiments/tralfamadorian97/runs/mecfs_runs.py
+++ experiments/tralfamadorian97/runs/mecfs_runs.py
@@ -3,8 +3,16 @@ Rough experimental scripts pertaining to analysis of ME/CFS data
 """
 
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
+from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.fine_mapping.with_palindromes.susie_finemap_decode_me_37_chr15_54_925_638_locus_plalindindromes import \
+    DECODE_ME_GWAS_37_CHR_15_54_925_638_FINEMAP_PALINDROMES
+from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.fine_mapping.with_palindromes.susie_finemap_decode_me_37_chr17_50_237_377_locus_palindromes import \
+    DECODE_ME_GWAS_37_CHR17_50_237_377_FINEMAP_PALINDROMES
+from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.fine_mapping.with_palindromes.susie_finemap_decode_me_37_chr1_174_128_548_locus_palindromes import \
+    DECODE_ME_GWAS_37_CHR1_174_128_548_FINEMAP_PALINDROMES
 from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.fine_mapping.with_palindromes.susie_finemap_decode_me_37_chr20_47_653_230_locus_palindromes import \
-    DECODE_ME_GWAS_37_CHR20_47_653_000_FINEMAP_PALNDROME
+    DECODE_ME_GWAS_37_CHR20_47_653_000_FINEMAP_PALNDROMES
+from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.fine_mapping.with_palindromes.susie_finemap_decode_me_37_chr6_26_215_000_locus_palindromes import \
+    DECODE_ME_GWAS_37_CHR6_26_215_000_FINEMAP_PALINDROMES
 from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.fine_mapping.with_palindromes.susie_finemap_decode_me_37_chr6_97_505_620_locus_palindromes import \
     DECODE_ME_GWAS_37_CHR6_97_505_620_FINEMAP_PALINDROMES
 
@@ -21,8 +29,17 @@ def run_initial_decode_me_analysis():
    # DECODE_ME_GWAS_37_CHR6_97_505_620_FINEMAP.terminal_tasks()
 
         #     DECODE_ME_GWAS_37_CHR6_26_215_000_FINEMAP.terminal_tasks(),
-        DECODE_ME_GWAS_37_CHR6_97_505_620_FINEMAP_PALINDROMES.terminal_tasks(),
-        # DECODE_ME_GWAS_37_CHR20_47_653_000_FINEMAP_PALNDROME.terminal_tasks(),
+        (
+        # DECODE_ME_GWAS_37_CHR1_174_128_548_FINEMAP_PALINDROMES.terminal_tasks()
+        #     DECODE_ME_GWAS_37_CHR6_26_215_000_FINEMAP_PALINDROMES.terminal_tasks()
+    DECODE_ME_GWAS_37_CHR6_97_505_620_FINEMAP_PALINDROMES.terminal_tasks()
+    #     DECODE_ME_GWAS_37_CHR6_97_505_620_FINEMAP_PALINDROMES.terminal_tasks()+
+    #     DECODE_ME_GWAS_37_CHR_15_54_925_638_FINEMAP_PALINDROMES.terminal_tasks()+
+    #     DECODE_ME_GWAS_37_CHR17_50_237_377_FINEMAP_PALINDROMES.terminal_tasks()+
+    #     DECODE_ME_GWAS_37_CHR20_47_653_000_FINEMAP_PALNDROMES.terminal_tasks()
+    ),
+
+    # DECODE_ME_GWAS_37_CHR20_47_653_000_FINEMAP_PALNDROME.terminal_tasks(),
         # MILLION_VETERAN_LDL_EUR_DATA_RAW
             # DECODE_ME_RABGAP1L_REGION_PLOT_37,
             # DECODE_ME_GWAS_1_37_ANNOVAR_DBSNP150_RSID_ASSIGNED.harmonize_task
