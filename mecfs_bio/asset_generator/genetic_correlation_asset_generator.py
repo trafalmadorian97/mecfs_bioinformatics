222745828f16708a65ea775e2ae2c7b9889f8a45
trafalmadorian97 <peterfor42@gmail.com>
2026-02-25 17:44:12 -0500

    wip

---
+++ mecfs_bio/asset_generator/genetic_correlation_asset_generator.py
@@ -0,0 +1,9 @@
+from typing import Sequence
+
+from mecfs_bio.build_system.task.gwaslab.gwaslab_genetic_corr_by_ct_ldsc_task import SumstatsSource
+
+
+def genetic_corr_by_ct_ldsc_asset_generator(
+        sources: Sequence[SumstatsSource],
+
+):
