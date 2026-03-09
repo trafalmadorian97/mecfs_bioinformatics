e61538a550f5d5d80a377037c8773fc184dca53c
trafalmadorian97 <peterfor42@gmail.com>
2026-03-09 15:46:47 -0400

    wip

--- experiments/tralfamadorian97/runs/lava_runs.py
+++ experiments/tralfamadorian97/runs/lava_runs.py
@@ -1,5 +1,6 @@
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
 from mecfs_bio.assets.gwas.multi_trait.genetic_correlation.lava.initial_lava import BASIC_G100_LAVA_ANALYSIS
+from mecfs_bio.assets.gwas.multi_trait.genetic_correlation.lava.initial_lava_ukbb import BASIC_UKBB_LAVA_ANALYSIS
 from mecfs_bio.assets.reference_data.lava_ld_reference.g1000_eur.processed.lava_thousand_geomes_eur_ld_ref_extracted import \
     LAVA_G100_EUR_LD_REF_EXTRACTED
 from mecfs_bio.assets.reference_data.lava_ld_reference.g1000_eur.raw.thousand_genomes_eur_ld_ref import \
@@ -11,7 +12,8 @@ from mecfs_bio.assets.reference_data.lava_locus_file.default.raw.default_lava_lo
 def run_lava():
     DEFAULT_RUNNER.run(
         [
-            LAVA_UKBB_LD_REF,
+            # LAVA_UKBB_LD_REF,
+            BASIC_UKBB_LAVA_ANALYSIS
             # LAVA_G100_EUR_LD_REF_EXTRACTED,
             # LAVA_G1000_EUR_REF_LD,
             # DEFAULT_LAVA_LOCUS_FILE
