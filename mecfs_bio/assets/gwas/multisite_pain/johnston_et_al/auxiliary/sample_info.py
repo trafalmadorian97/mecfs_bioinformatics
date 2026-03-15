7d23a1c8d9e8cda15b5cd414111209552ba3cb13
trafalmadorian97 <peterfor42@gmail.com>
2026-03-15 19:43:14 -0400

    wip

--- mecfs_bio/assets/gwas/multisite_pain/johnston_et_al/auxiliary/sample_info.py
+++ mecfs_bio/assets/gwas/multisite_pain/johnston_et_al/auxiliary/sample_info.py
@@ -2,4 +2,4 @@ from mecfs_bio.build_system.task.gwaslab.gwaslab_genetic_corr_by_ct_ldsc_task im
     QuantPhenotype,
 )
 
-JOHNSTON_ET_AL_SAMPLE_INFO = QuantPhenotype(total_sample_size=387649)
+JOHNSTON_ET_AL_SAMPLE_INFO = QuantPhenotype(total_sample_size=387649) # True total sample size. From Gwas catalog
