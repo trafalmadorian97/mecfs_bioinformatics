763f305d72b8d8660b71dcfb7c56c0a96ef6ce43
trafalmadorian97 <peterfor42@gmail.com>
2026-03-29 19:10:39 -0400

    add_pipe_for_float_formatting

--- experiments/tralfamadorian97/for_documentation/bivariate_mixer_table_gen.py
+++ experiments/tralfamadorian97/for_documentation/bivariate_mixer_table_gen.py
@@ -1,10 +1,16 @@
+from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.mixer.decode_me_univariate_mixer import DECODE_ME_UNIVARIATE_MIXER
 from mecfs_bio.assets.gwas.multi_trait.polygenic_overlap.bivariate_mixer.mecfs_pain_bivariate_mixer import \
     MECFS_PAIN_BIVARIATE_MIXER
+from mecfs_bio.assets.gwas.multisite_pain.johnston_et_al.analysis.mixer.johnston_et_al_univariate_mixer import \
+    JOHNSTON_ET_AL_UNIVARIATE_MIXER
 from mecfs_bio.figures.key_scripts.generate_figures import generate_figures
 
 
 def go():
-    generate_figures([MECFS_PAIN_BIVARIATE_MIXER.result_table_markdown_task])
+    generate_figures([MECFS_PAIN_BIVARIATE_MIXER.result_table_markdown_task,
+                      DECODE_ME_UNIVARIATE_MIXER.result_markdown_table_task,
+                      JOHNSTON_ET_AL_UNIVARIATE_MIXER.result_markdown_table_task
+                      ])
 
 
 if __name__ == '__main__':
