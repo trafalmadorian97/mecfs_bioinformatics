d44adac4c643b2a48c15a18ac8e25e3a2dd8eb0b
trafalmadorian97 <peterfor42@gmail.com>
2026-03-30 16:25:25 -0400

    start LCV task

---
+++ mecfs_bio/build_system/task/lcv/lcv_task.py
@@ -0,0 +1,23 @@
+"""
+Task to  
+"""
+from pathlib import Path
+
+from mecfs_bio.build_system.asset.base_asset import Asset
+from mecfs_bio.build_system.meta.meta import Meta
+from mecfs_bio.build_system.rebuilder.fetch.base_fetch import Fetch
+from mecfs_bio.build_system.task.base_task import Task
+from mecfs_bio.build_system.wf.base_wf import WF
+
+
+class LCVTask(Task):
+    @property
+    def meta(self) -> Meta:
+        pass
+
+    @property
+    def deps(self) -> list["Task"]:
+        pass
+
+    def execute(self, scratch_dir: Path, fetch: Fetch, wf: WF) -> Asset:
+        pass
