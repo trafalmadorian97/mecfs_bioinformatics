abfa74175058b8943197c2d5a5f0ff6321d1665f
trafalmadorian97 <peterfor42@gmail.com>
2026-03-30 16:48:27 -0400

    work on LCV task

--- mecfs_bio/build_system/task/lcv/lcv_task.py
+++ mecfs_bio/build_system/task/lcv/lcv_task.py
@@ -1,7 +1,16 @@
 """
-Task to  
+Task to apply the Latent Causal Variable technique of O'Connor and Price to attempt
+to estimate the causal director between two genetically correlated traits.
+The key output value is GCP: Genetic Causality Proportion.
+
+Citation:
+O’Connor, Luke J., and Alkes L. Price. "Distinguishing genetic correlation
+from causation across 52 diseases and complex traits." Nature genetics 50.12 (2018): 1728-1734.
+
 """
 from pathlib import Path
+
+from attrs import frozen
 
 from mecfs_bio.build_system.asset.base_asset import Asset
 from mecfs_bio.build_system.meta.meta import Meta
@@ -10,14 +19,27 @@ from mecfs_bio.build_system.task.base_task import Task
 from mecfs_bio.build_system.wf.base_wf import WF
 
 
+@frozen
 class LCVTask(Task):
+    """
+    Task to apply the Latent Causal Variable technique of O'Connor and Price to attempt
+    to estimate the causal director between two genetically correlated traits.
+    The key output value is GCP: Genetic Causality Proportion.
+
+    """
+    _meta: Meta
+    trait_1_data: Task
+    trait_2_data: Task
+
     @property
     def meta(self) -> Meta:
-        pass
+        return self._meta
 
     @property
     def deps(self) -> list["Task"]:
-        pass
+        return [self.trait_1_data, self.trait_2_data]
 
     def execute(self, scratch_dir: Path, fetch: Fetch, wf: WF) -> Asset:
-        pass
+        trait_1_asset = fetch(self.trait_1_data.asset_id)
+        trait_2_asset = fetch(self.trait_2_data.asset_id)
+        #wip
