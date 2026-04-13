eb43dad50610e9917d404de60ef6d5d111c946bd
trafalmadorian97 <peterfor42@gmail.com>
2026-04-13 15:02:08 -0400

    run ldsc on height

---
+++ mecfs_bio/assets/gwas/height/yengo_2022/analysis/yengo_standard_analysis.py
@@ -0,0 +1,38 @@
+from mecfs_bio.asset_generator.concrete_standard_analysis_task_generator import \
+    concrete_standard_analysis_generator_assume_already_has_rsid
+from mecfs_bio.assets.gwas.height.yengo_2022.raw.yengo_height_raw import YENGO_ET_AL_RAW
+from mecfs_bio.build_system.task.gwaslab.gwaslab_create_sumstats_task import GWASLabColumnSpecifiers
+from mecfs_bio.build_system.task.gwaslab.gwaslab_genetic_corr_by_ct_ldsc_task import QuantPhenotype
+from mecfs_bio.build_system.task.magma.magma_plot_brain_atlas_result_with_stepwise_labels import HBAIndepPlotOptions
+from mecfs_bio.build_system.task.magma.plot_magma_brain_atlas_result import PlotSettings
+from mecfs_bio.build_system.task.pipes.filter_rows_by_min_in_col import FilterRowsByMinInCol
+from mecfs_bio.build_system.task.pipes.filter_rows_by_value import FilterRowsByValue
+
+YENGO_STANDARD_ANALYSIS = (
+    concrete_standard_analysis_generator_assume_already_has_rsid(
+        base_name="yengo_et_al_height",
+        raw_gwas_data_task=YENGO_ET_AL_RAW,
+        sample_size= int(1.587709e+06),  # sample size varies by SNP.  This is the median
+        fmt=GWASLabColumnSpecifiers(
+            rsid="RSID",
+            chrom="CHR",
+            pos="POS",
+            ea="EFFECT_ALLELE",
+            nea="OTHER_ALLELE",
+            eaf="EFFECT_ALLELE_FREQ",
+            beta="BETA",
+            p="P",
+            se="SE"
+        ),
+
+        include_hba_magma_tasks=True,
+        hba_plot_settings=PlotSettings("plotly_white"),
+        include_independent_cluster_plot_in_hba=True,
+        hba_indep_plot_options=HBAIndepPlotOptions(annotation_text_size=11),
+        pre_pipe=FilterRowsByMinInCol(
+            col="N",
+            min_value=1.032694e+06,
+        ),
+        phenotype_info_for_ldsc=QuantPhenotype()
+    )
+)
---
+++ mecfs_bio/assets/gwas/height/yengo_2022/raw/yengo_height_raw.py
@@ -0,0 +1,19 @@
+from pathlib import PurePath
+
+from mecfs_bio.build_system.meta.asset_id import AssetId
+from mecfs_bio.build_system.meta.gwas_summary_file_meta import GWASSummaryDataFileMeta
+from mecfs_bio.build_system.meta.read_spec.dataframe_read_spec import DataFrameTextFormat, DataFrameReadSpec
+from mecfs_bio.build_system.task.download_file_task import DownloadFileTask
+
+YENGO_ET_AL_RAW = DownloadFileTask(
+    meta=GWASSummaryDataFileMeta(
+        id=AssetId("yengo_height_raw"),
+        trait="height",
+        project="yengo_et_al_2022",
+        sub_dir="raw",
+        project_path=PurePath("GIANT_HEIGHT_YENGO_2022_GWAS_SUMMARY_STATS_EUR.gz"),
+        read_spec=DataFrameReadSpec(DataFrameTextFormat(separator="\t")),
+    ),
+    url="https://giant-consortium.web.broadinstitute.org/images/f/f7/GIANT_HEIGHT_YENGO_2022_GWAS_SUMMARY_STATS_EUR.gz",
+    md5_hash=None,
+)
