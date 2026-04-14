ecb44dc373d81234dc319280e4c029520c125f0c
trafalmadorian97 <peterfor42@gmail.com>
2026-04-14 11:14:07 -0400

    docstring

--- mecfs_bio/assets/gwas/height/yengo_2022/raw/yengo_height_raw.py
+++ mecfs_bio/assets/gwas/height/yengo_2022/raw/yengo_height_raw.py
@@ -1,3 +1,15 @@
+"""
+Task to pull raw data from Yengo et al.'s 2022 GWAS of adult height.
+Citation:
+Yengo, Loïc, et al. "A saturated map of common genetic variants associated
+ with human height." Nature 610.7933 (2022): 704-712.
+
+Giant consortium data page: https://giant-consortium.web.broadinstitute.org/index.php/GIANT_consortium_data_files
+
+
+
+"""
+
 from pathlib import PurePath
 
 from mecfs_bio.build_system.meta.asset_id import AssetId
@@ -18,5 +30,5 @@ YENGO_ET_AL_RAW = DownloadFileTask(
         read_spec=DataFrameReadSpec(DataFrameTextFormat(separator="\t")),
     ),
     url="https://giant-consortium.web.broadinstitute.org/images/f/f7/GIANT_HEIGHT_YENGO_2022_GWAS_SUMMARY_STATS_EUR.gz",
-    md5_hash=None,
+    md5_hash="710ef5ac573e9023b56ec37b959a4e29",
 )
