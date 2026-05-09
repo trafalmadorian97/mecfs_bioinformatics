b315bad9c9f6d0eb60e2d1d2a1c10b4c8c45ecbe
trafalmadorian97 <peterfor42@gmail.com>
2026-05-09 17:34:18 -0400

    cfs_analysis

--- mecfs_bio/assets/gwas/me_cfs/million_veterans/analysis/million_veterans_cfs_standard_analysis.py
+++ mecfs_bio/assets/gwas/me_cfs/million_veterans/analysis/million_veterans_cfs_standard_analysis.py
@@ -3,6 +3,9 @@ from mecfs_bio.asset_generator.concrete_standard_analysis_task_generator import 
 from mecfs_bio.assets.gwas.me_cfs.million_veterans.auxillary.pheotype_info import MILLION_VETERAN_CFS_PHENOTYPE_INFO
 from mecfs_bio.assets.gwas.me_cfs.million_veterans.raw.million_veterans_cfs_download import MILLION_VETERANS_CFS_RAW
 from mecfs_bio.build_system.task.gwaslab.gwaslab_create_sumstats_task import GWASLabColumnSpecifiers
+from mecfs_bio.build_system.task.pipes.composite_pipe import CompositePipe
+from mecfs_bio.build_system.task.pipes.compute_beta_pipe import ComputeBetaIfNeededPipe
+from mecfs_bio.build_system.task.pipes.compute_se_pipe import ComputeSEPipe
 
 MILLION_VETERANS_CFS_STANDARD_ANALYSIS_TASK_GROUP= concrete_standard_analysis_generator_assume_already_has_rsid(
     base_name="million_veterans_cfs",
@@ -21,5 +24,7 @@ MILLION_VETERANS_CFS_STANDARD_ANALYSIS_TASK_GROUP= concrete_standard_analysis_ge
     sample_size=443093, # from sumstats file
     manhattan_settings=ManhattanPlotSettings(),
     phenotype_info_for_ldsc=MILLION_VETERAN_CFS_PHENOTYPE_INFO,
+    pre_sldsc_pipe=CompositePipe([ComputeBetaIfNeededPipe(),
+                                  ComputeSEPipe()]),
 
 )
