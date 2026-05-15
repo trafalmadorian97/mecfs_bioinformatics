f4a7fed75e65da3ad58b16c528332e8903970cfb
trafalmadorian97 <peterfor42@gmail.com>
2026-05-15 09:43:26 -0400

    implement generate_new_figures

--- mecfs_bio/figures/key_scripts/generate_new_figures.py
+++ mecfs_bio/figures/key_scripts/generate_new_figures.py
@@ -2,20 +2,35 @@ from pathlib import Path
 
 import structlog
 
-from mecfs_bio.build_system.meta.gwaslab_meta.gwaslab_manhattan_plot_meta import GWASLabManhattanQQPlotMeta
+from mecfs_bio.build_system.meta.gwaslab_meta.gwaslab_manhattan_plot_meta import (
+    GWASLabManhattanQQPlotMeta,
+)
 from mecfs_bio.build_system.meta.markdown_file_meta import MarkdownFileMeta
 from mecfs_bio.build_system.meta.plot_file_meta import GWASPlotFileMeta
 from mecfs_bio.build_system.meta.plot_meta import GWASPlotDirectoryMeta
 from mecfs_bio.build_system.task.base_task import Task
 from mecfs_bio.figures.fig_constants import FIGURE_DIRECTORY
-from mecfs_bio.figures.figure_exporter import get_fig_file_path, get_fig_dir_meta, get_md_fig_file_path
+from mecfs_bio.figures.figure_exporter import (
+    FigureExporter,
+    get_fig_dir_meta,
+    get_fig_file_path,
+    get_md_fig_file_path,
+)
 from mecfs_bio.figures.figure_task_list import ALL_FIGURE_TASKS
 from mecfs_bio.figures.key_scripts.generate_figures import FIGURE_EXPORTER
 
-
 logger = structlog.get_logger()
 
-def generate_new_figures(all_figure_tasks: list[Task],fig_dir:Path):
+
+def generate_new_figures(
+    all_figure_tasks: list[Task],
+    fig_dir: Path,
+    exporter: FigureExporter,
+):
+    """
+    Aim of this function is to only invoke the build system  to build figures that do not
+    currently exist in the figure directory.
+    """
     tasks_to_run = []
     for task in all_figure_tasks:
         meta = task.meta
@@ -30,10 +45,13 @@ def generate_new_figures(all_figure_tasks: list[Task],fig_dir:Path):
         if not dst.exists():
             tasks_to_run.append(task)
 
-    structlog.debug(f"Found new figure tasks: {tasks_to_run}")
-    FIGURE_EXPORTER.export(tasks_to_run, fig_dir=fig_dir)
+    logger.debug(f"Found new figure tasks: {tasks_to_run}")
+    exporter.export(tasks_to_run, fig_dir=fig_dir)
 
 
 if __name__ == "__main__":
-    generate_new_figures(all_figure_tasks=ALL_FIGURE_TASKS,fig_dir=FIGURE_DIRECTORY)
-
+    generate_new_figures(
+        all_figure_tasks=ALL_FIGURE_TASKS,
+        fig_dir=FIGURE_DIRECTORY,
+        exporter=FIGURE_EXPORTER,
+    )
