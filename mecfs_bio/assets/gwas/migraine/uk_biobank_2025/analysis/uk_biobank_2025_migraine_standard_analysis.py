c3b5abc57fd60ed15d476e860aa22d776e7940b1
ruvilonix <114708614+ruvilonix@users.noreply.github.com>
2026-05-25 14:49:16 -0400

    finished migraine standard analysis

--- mecfs_bio/assets/gwas/migraine/uk_biobank_2025/analysis/uk_biobank_2025_migraine_standard_analysis.py
+++ mecfs_bio/assets/gwas/migraine/uk_biobank_2025/analysis/uk_biobank_2025_migraine_standard_analysis.py
@@ -1,8 +1,8 @@
 from mecfs_bio.asset_generator.concrete_standard_analysis_task_generator import (
     concrete_standard_analysis_generator_assume_already_has_rsid,
 )
-from mecfs_bio.assets.gwas.migraine.uk_biobank_2025.raw.uk_biobank_2025_migraine_raw import (
-    UK_BIOBANK_2025_MIGRAINE_EUR_DATA_RAW,
+from mecfs_bio.assets.gwas.migraine.uk_biobank_2025.processed.uk_biobank_2025_migraine_common_snps import (
+    UK_BIOBANK_2025_MIGRAINE_EUR_COMMON_SNPS_TASK,
 )
 from mecfs_bio.build_system.task.gwaslab.gwaslab_create_sumstats_task import (
     GWASLabColumnSpecifiers,
@@ -10,13 +10,10 @@ from mecfs_bio.build_system.task.gwaslab.gwaslab_create_sumstats_task import (
 from mecfs_bio.build_system.task.gwaslab.gwaslab_genetic_corr_by_ct_ldsc_task import (
     BinaryPhenotypeSampleInfo,
 )
-from mecfs_bio.build_system.task.pipes.composite_pipe import CompositePipe
-from mecfs_bio.build_system.task.pipes.compute_beta_pipe import ComputeBetaPipe
-from mecfs_bio.build_system.task.pipes.compute_se_pipe import ComputeSEPipe
 
 UK_BIOBANK_2025_EUR_MIGRAINE_STANDARD_ANALYSIS = concrete_standard_analysis_generator_assume_already_has_rsid(
     base_name="uk_biobank_2025_migraine_eur",
-    raw_gwas_data_task=UK_BIOBANK_2025_MIGRAINE_EUR_DATA_RAW,
+    raw_gwas_data_task=UK_BIOBANK_2025_MIGRAINE_EUR_COMMON_SNPS_TASK,
     fmt=GWASLabColumnSpecifiers(
         chrom="chromosome",
         pos="base_pair_location",
@@ -33,5 +30,4 @@ UK_BIOBANK_2025_EUR_MIGRAINE_STANDARD_ANALYSIS = concrete_standard_analysis_gene
         sample_prevalence=25393 / 458440,  # from gwas catalog page
         estimated_population_prevalence=0.104,  # UK prevalence from https://pmc.ncbi.nlm.nih.gov/articles/PMC11753071/
     ),
-    pre_sldsc_pipe=CompositePipe([ComputeBetaPipe(), ComputeSEPipe()]),
 )
---
+++ mecfs_bio/build_system/task/filter_snps_by_frequency.py
@@ -0,0 +1,81 @@
+"""
+Task to filter SNPs in a GWAS according to another table of SNPs passing quality control.
+Implemented as a DataFrame join.
+"""
+
+from pathlib import Path, PurePath
+
+import structlog
+from attrs import frozen
+
+from mecfs_bio.build_system.asset.file_asset import FileAsset
+from mecfs_bio.build_system.meta.asset_id import AssetId
+from mecfs_bio.build_system.meta.filtered_gwas_data_meta import FilteredGWASDataMeta
+from mecfs_bio.build_system.meta.gwas_summary_file_meta import GWASSummaryDataFileMeta
+from mecfs_bio.build_system.meta.read_spec.dataframe_read_spec import (
+    DataFrameParquetFormat,
+    DataFrameReadSpec,
+)
+from mecfs_bio.build_system.meta.read_spec.read_dataframe import (
+    scan_dataframe_asset,
+)
+from mecfs_bio.build_system.rebuilder.fetch.base_fetch import Fetch
+from mecfs_bio.build_system.task.base_task import Task
+from mecfs_bio.build_system.wf.base_wf import WF
+
+logger = structlog.get_logger()
+
+import narwhals as nw
+
+
+@frozen
+class FilterSNPsFrequencyTask(Task):
+    raw_gwas_task: Task
+    meta: FilteredGWASDataMeta
+    allele_freq_col: str = "effect_allele_frequency"
+    freq_thresh: float = 0.05
+
+    @property
+    def deps(self) -> list["Task"]:
+        return [self.raw_gwas_task]
+
+    def execute(self, scratch_dir: Path, fetch: Fetch, wf: WF) -> FileAsset:
+        logger.debug(f"Filtering to common variants (MAF >= {self.freq_thresh})")
+        df = scan_dataframe_asset(
+            asset=fetch(self.raw_gwas_task.asset_id),
+            meta=self.raw_gwas_task.meta,
+        )
+        result = df.filter(
+            nw.min_horizontal(
+                nw.col(self.allele_freq_col), 1 - nw.col(self.allele_freq_col)
+            )
+            >= self.freq_thresh
+        )
+        target_path = scratch_dir / "tmp.parqet"
+        result.sink_parquet(target_path)
+        return FileAsset(target_path)
+
+    @classmethod
+    def create(
+        cls,
+        id: str,
+        raw_gwas_task: Task,
+        allele_freq_col: str = "effect_allele_frequency",
+        freq_thresh: float = 0.05,
+    ) -> Task:
+        source_meta = raw_gwas_task.meta
+        assert isinstance(source_meta, GWASSummaryDataFileMeta)
+        meta = FilteredGWASDataMeta(
+            id=AssetId(id),
+            trait=source_meta.trait,
+            project=source_meta.project,
+            sub_dir=PurePath("processed"),
+            extension=".parquet",
+            read_spec=DataFrameReadSpec(format=DataFrameParquetFormat()),
+        )
+        return cls(
+            raw_gwas_task=raw_gwas_task,
+            meta=meta,
+            allele_freq_col=allele_freq_col,
+            freq_thresh=freq_thresh,
+        )
