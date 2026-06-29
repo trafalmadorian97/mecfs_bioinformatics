d98eba725b1da11f168ef847bb8e51cda8e38a07
trafalmadorian97 <peterfor42@gmail.com>
2026-06-29 13:46:36 -0400

    wip

--- mecfs_bio/analysis/decode_me_pain_subtraction_ols.py
+++ mecfs_bio/analysis/decode_me_pain_subtraction_ols.py
@@ -12,12 +12,12 @@ def run_decode_me_minus_pain_ols():
     DEFAULT_RUNNER.run(
         # DECODE_ME_MINUS_PAIN_OLS_GENETIC_CORR_GENERATOR.terminal_tasks()
         # + DECODE_ME_MINUS_PAIN_OLS_STANDARD_ANALYSIS.get_terminal_tasks()
-        [
+        # [
             # DECODE_ME_MINUS_PAIN_OLS_STANDARD_ANALYSIS.magma_gene_manhattan_plot_unwrap
-            DECODE_ME_MINUS_PAIN_OLS_STANDARD_ANALYSIS.hba_magma_tasks_unwrap.magma_independent_cluster_plot_unwrap
-        ],
+            DECODE_ME_MINUS_PAIN_OLS_STANDARD_ANALYSIS.h_magma_tasks.terminal_tasks()
+        ,
         must_rebuild_transitive=[
-            DECODE_ME_MINUS_PAIN_OLS_STANDARD_ANALYSIS.hba_magma_tasks_unwrap.magma_independent_cluster_plot_unwrap.stepwise_cluster_list_task
+            # DECODE_ME_MINUS_PAIN_OLS_STANDARD_ANALYSIS.hba_magma_tasks_unwrap.magma_independent_cluster_plot_unwrap.stepwise_cluster_list_task
         ],
     )
 
