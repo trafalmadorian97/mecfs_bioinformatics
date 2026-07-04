ce3939c2866165fb95f76179cc351f2e3cd1961d
trafalmadorian97 <peterfor42@gmail.com>
2026-07-04 14:02:04 -0400

    edit_comments

--- mecfs_bio/assets/gwas/rheumtoid_arthritis/decode_seropositive/raw/download_raw_ra_seropositive.py
+++ mecfs_bio/assets/gwas/rheumtoid_arthritis/decode_seropositive/raw/download_raw_ra_seropositive.py
@@ -1,3 +1,10 @@
+"""
+Task to download DECODE GWAS of seropositive Rheumatoid arthritis.
+Citation:
+Saevarsdottir, Saedis, et al. "Multiomics analysis of rheumatoid arthritis yields sequence variants that have large effects on risk of the seropositive subset."i
+Annals of the rheumatic diseases81.8 (2022): 1085-1095.
+"""
+
 from pathlib import PurePath
 
 from mecfs_bio.build_system.meta.asset_id import AssetId
@@ -24,5 +31,5 @@ DECODE_SEROPOSITIVE_RA_RAW = DownloadFileTask(
     # url="https://www.dropbox.com/scl/fi/8uumw9pwphkyjl2jru03u/RA_Seronegative.txt.gz?rlkey=ad35vhuaeo6oouqsam2nrcxvn&dl=1",
     url="https://www.dropbox.com/scl/fi/d5vxmi04gfiwx8m52xkgh/RA_Seropositive.txt.gz?rlkey=jbo7fvwf27yfdaoodttqo7rld&dl=1",
     # md5_hash="b5c7c41cbd85be11dbc63f5f0402e40f",
-    md5_hash=None,
+    md5_hash="29efd80821436d21d1d185e11ff484c8",
 )
