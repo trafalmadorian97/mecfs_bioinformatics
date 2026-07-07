34b5fc6eb388f3469f54f234b8850238ec354bbd
Tralfamadoran97 <peterfor42@gmail.com>
2026-07-07 17:32:54 -0400

    Document RA results (#881)
    - Continue write up of seropositive RA GWAS, including MAGMA and S-LDSC
    analysis

--- experiments/tralfamadorian97/runs/ra_runs.py
+++ experiments/tralfamadorian97/runs/ra_runs.py
@@ -1,6 +1,6 @@
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
 from mecfs_bio.assets.gwas.rheumtoid_arthritis.decode_seropositive.analysis.ra_seropositive_standard_analysis import \
-    DECODE_SEROPOSITIVE_RA_STANDARD_ANALYSIS
+    SEROPOSITIVE_RA_STANDARD_ANALYSIS
 from mecfs_bio.assets.gwas.rheumtoid_arthritis.decode_seropositive.processed.ra_seropositive_preprocess import \
     SEROPOS_RA_FILTERED_FOR_FREQ
 from mecfs_bio.assets.gwas.rheumtoid_arthritis.decode_seropositive.raw.download_raw_ra_seropositive import \
@@ -9,7 +9,7 @@ from mecfs_bio.assets.gwas.rheumtoid_arthritis.decode_seropositive.raw.download_
 
 def go():
     DEFAULT_RUNNER.run(
-        targets=DECODE_SEROPOSITIVE_RA_STANDARD_ANALYSIS.terminal_tasks(),
+        targets=SEROPOSITIVE_RA_STANDARD_ANALYSIS.tasks.magma_tasks.inner.terminal_tasks(),
         must_rebuild_transitive=[
             # SEROPOS_RA_FILTERED_FOR_FREQ,
         ],
