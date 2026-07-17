97ebd67357e0e7ec6ce466247bbaf57740491d35
trafalmadorian97 <peterfor42@gmail.com>
2026-07-17 18:04:19 -0400

    wip

--- experiments/tralfamadorian97/runs/ppp_ldsc_runs.py
+++ experiments/tralfamadorian97/runs/ppp_ldsc_runs.py
@@ -1,11 +1,13 @@
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
-from mecfs_bio.assets.gwas.ukbb_ppp.ppp_database.hapmap3.hapmap3_ppp_heritability import PPP_SAMPLE_SIZES
+from mecfs_bio.assets.gwas.ukbb_ppp.ppp_database.hapmap3.hapmap3_ppp_heritability import PPP_SAMPLE_SIZES, \
+    HAPMAP_3_PPP_HERITABILITY
 
 
 def go():
     DEFAULT_RUNNER.run(
         [
-            PPP_SAMPLE_SIZES
+            PPP_SAMPLE_SIZES,
+            HAPMAP_3_PPP_HERITABILITY
         ]
     )
 
