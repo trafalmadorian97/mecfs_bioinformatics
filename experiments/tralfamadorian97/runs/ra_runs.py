ebaf320c8af553090a486a2aecb2e6bd40e2fde6
trafalmadorian97 <peterfor42@gmail.com>
2026-07-21 14:44:09 -0400

    add cis excluded run

--- experiments/tralfamadorian97/runs/ra_runs.py
+++ experiments/tralfamadorian97/runs/ra_runs.py
@@ -1,21 +1,25 @@
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
 from mecfs_bio.assets.gwas.rheumtoid_arthritis.decode_seropositive.analysis.ra_seropositive_ppp_rg import \
-    SEROPOSITIVE_RA_PPP_RG_TASKS
+    SEROPOSITIVE_RA_PPP_RG_TASKS, SEROPOSITIVE_RA_PPP_RG_TASKS_CIS_EXCLUDED
 from mecfs_bio.assets.gwas.rheumtoid_arthritis.decode_seropositive.analysis.ra_seropositive_standard_analysis import \
     SEROPOSITIVE_RA_STANDARD_ANALYSIS
 from mecfs_bio.assets.gwas.rheumtoid_arthritis.decode_seropositive.processed.ra_seropositive_preprocess import \
     SEROPOS_RA_FILTERED_FOR_FREQ
 from mecfs_bio.assets.gwas.rheumtoid_arthritis.decode_seropositive.raw.download_raw_ra_seropositive import \
     DECODE_SEROPOSITIVE_RA_RAW
+from mecfs_bio.assets.reference_data.linkage_disequilibrium_score_reference_data.extracted.eur_ld_scores_thousand_genomes_phase_3_v1_consolidated import \
+    THOUSAND_GENOME_EUR_LD_REFERENCE_DATA_V1_CONSOLIDATE
 
 
 def go():
     DEFAULT_RUNNER.run(
         targets=[
             # SEROPOSITIVE_RA_STANDARD_ANALYSIS.tasks.heritability_markdown_task_unwrap
-            SEROPOSITIVE_RA_PPP_RG_TASKS.rg_task
+            # SEROPOSITIVE_RA_PPP_RG_TASKS.rg_task
+            SEROPOSITIVE_RA_PPP_RG_TASKS_CIS_EXCLUDED.rg_task
         ],
         must_rebuild_transitive=[
+            THOUSAND_GENOME_EUR_LD_REFERENCE_DATA_V1_CONSOLIDATE
             # SEROPOS_RA_FILTERED_FOR_FREQ,
         ],
     )
--- mecfs_bio/assets/gwas/rheumtoid_arthritis/decode_seropositive/analysis/ra_seropositive_ppp_rg.py
+++ mecfs_bio/assets/gwas/rheumtoid_arthritis/decode_seropositive/analysis/ra_seropositive_ppp_rg.py
@@ -5,9 +5,19 @@ from mecfs_bio.assets.gwas.rheumtoid_arthritis.decode_seropositive.analysis.ra_s
 from mecfs_bio.build_system.task.pipes.composite_pipe import CompositePipe
 from mecfs_bio.build_system.task.pipes.compute_beta_pipe import ComputeBetaPipe
 from mecfs_bio.build_system.task.pipes.compute_se_pipe import ComputeSEPipe
+from mecfs_bio.build_system.task.ppp_ldsc.ppp_protein_rg_task import PppRgConfig
 
 SEROPOSITIVE_RA_PPP_RG_TASKS = generate_ppp_rg_assets(
     "seropositive_ra_ppp_rg",
     trait_task=SEROPOSITIVE_RA_STANDARD_ANALYSIS.assign_rsids_task_group.join_task,
     trait_pipe=CompositePipe([ComputeBetaPipe(), ComputeSEPipe()]),
+)
+
+SEROPOSITIVE_RA_PPP_RG_TASKS_CIS_EXCLUDED = generate_ppp_rg_assets(
+    "seropositive_ra_ppp_rg_cis_excluded",
+    trait_task=SEROPOSITIVE_RA_STANDARD_ANALYSIS.assign_rsids_task_group.join_task,
+    trait_pipe=CompositePipe([ComputeBetaPipe(), ComputeSEPipe()]),
+    config=PppRgConfig(
+        variant_set="cis_excluded"
+    ),
 )
