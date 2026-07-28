59ae0758fdb1ce1ab60b8833c385eebc9592b65a
trafalmadorian97 <peterfor42@gmail.com>
2026-07-28 16:40:03 -0400

    write up migraine genetic corr results

--- experiments/tralfamadorian97/runs/migraine_ct_ldsc_runs.py
+++ experiments/tralfamadorian97/runs/migraine_ct_ldsc_runs.py
@@ -1,10 +1,10 @@
 from mecfs_bio.analysis.runner.default_runner import DEFAULT_RUNNER
-from mecfs_bio.assets.gwas.migraine.multistudy.ct_ldsc_migraine_studies import MIGRAINE_CT_LDSC_ASSET_GENERATOR
+from mecfs_bio.assets.gwas.migraine.multistudy.ct_ldsc_migraine_studies import MIGRAINE_CROSS_STUDY_CT_LDSC_ASSET_GENERATOR
 
 
 def go():
     DEFAULT_RUNNER.run(
-        MIGRAINE_CT_LDSC_ASSET_GENERATOR.terminal_tasks()
+        MIGRAINE_CROSS_STUDY_CT_LDSC_ASSET_GENERATOR.terminal_tasks()
     )
 
 if __name__ == '__main__':
--- mecfs_bio/assets/gwas/migraine/million_veterans/analysis/million_veterans_migraine_standard_analysis.py
+++ mecfs_bio/assets/gwas/migraine/million_veterans/analysis/million_veterans_migraine_standard_analysis.py
@@ -14,12 +14,11 @@ from mecfs_bio.build_system.task.pipes.composite_pipe import CompositePipe
 from mecfs_bio.build_system.task.pipes.compute_beta_pipe import ComputeBetaPipe
 from mecfs_bio.build_system.task.pipes.compute_se_pipe import ComputeSEPipe
 
-
-MILLION_VETERANS_MIGRAINE_SAMPLE_INFO= BinaryPhenotypeSampleInfo(
-            sample_prevalence=31836 / 437667,  # from gwas catalog page
-            estimated_population_prevalence=0.14,  # from Google search
-    total_sample_size=437667
-        )
+MILLION_VETERANS_MIGRAINE_SAMPLE_INFO = BinaryPhenotypeSampleInfo(
+    sample_prevalence=31836 / 437667,  # from gwas catalog page
+    estimated_population_prevalence=0.14,  # from Google search
+    total_sample_size=437667,
+)
 
 MILLION_VETERANS_EUR_MIGRAINE_STANDARD_ANALYSIS = (
     concrete_standard_analysis_generator_assume_already_has_rsid(
