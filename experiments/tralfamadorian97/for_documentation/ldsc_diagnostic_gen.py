eca10469329f14094e4f61e93283e38f54a1616a
trafalmadorian97 <peterfor42@gmail.com>
2026-09-15 12:30:30 -0400

    wip

--- experiments/tralfamadorian97/for_documentation/ldsc_diagnostic_gen.py
+++ experiments/tralfamadorian97/for_documentation/ldsc_diagnostic_gen.py
@@ -1,5 +1,6 @@
 from mecfs_bio.assets.gwas.me_cfs.decode_me.analysis.decode_me_gwas_1_ldsc_diagnostic_plot import \
     DECODE_ME_GWAS_1_LDSC_DIAGNOSTIC_PLOT
+from mecfs_bio.figures.key_scripts.push_figures import push_figures
 from mecfs_bio.figures.key_scripts.regenerate_figures import regenerate_figures
 
 
@@ -7,6 +8,7 @@ def go():
     regenerate_figures(
         [DECODE_ME_GWAS_1_LDSC_DIAGNOSTIC_PLOT]
     )
+    push_figures()
 
 if __name__ == '__main__':
     go()
